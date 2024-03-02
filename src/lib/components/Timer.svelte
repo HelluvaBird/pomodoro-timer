2396ac2247a3cefe030d56d45a320b5c2d7173cf
HelluvaBird <royceman24@hotmail.com>
2024-03-02 14:33:33 -0500

    add border to Timer component

--- src/lib/components/Timer.svelte
+++ src/lib/components/Timer.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import { settings } from '$lib/store';
+	import { getProgressColor } from '$lib/utils/utils';
 	import { onMount } from 'svelte';
 
 	let timer = '00:00';
 	let isPaused = true;
 	let timerId: number;
 	let time = $settings.pomodoro;
+	let percentage = '0';
 
 	function formatTime(time: number) {
 		const minutes = Math.floor(time / 60);
@@ -14,10 +16,24 @@
 		timer = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 	}
 
+	function formatPercentage(time: number) {
+		let total;
+		if (!$settings.isBreak) {
+			total = $settings.pomodoro;
+		} else if ($settings.isBreak && $settings.breaks < 4) {
+			total = $settings.short;
+		} else {
+			total = $settings.long;
+		}
+
+		percentage = (Math.ceil((time / total) * 10) * 10).toString();
+	}
+
 	function startTimer() {
 		timerId = setInterval(() => {
 			time--;
 			formatTime(time);
+			formatPercentage(time);
 			if (time < 0) {
 				completePomodoro();
 			}
@@ -47,6 +63,7 @@
 			$settings.isBreak = true;
 		}
 		formatTime(time);
+		formatPercentage(time);
 		startTimer();
 	}
 
@@ -61,17 +78,24 @@
 
 	onMount(() => {
 		formatTime(time);
+		formatPercentage(time);
 		() => clearInterval(timerId);
 	});
+
+	$: background = `radial-gradient(closest-side, rgb(30 41 59) 79%, transparent 80% 90%, rgb(30 41 59) 91% 100% ), conic-gradient(${getProgressColor($settings.color)} ${percentage}%, rgb(30 41 59) 0)`;
 </script>
 
 <div
 	class="mx-auto bg-slate-800 shadow-lg shadow-white/50 size-96 rounded-full grid relative place-items-center"
 >
+	<button
+		class="relative text-4xl tracking-[0.3em] w-full h-full rounded-full"
+		style:background
+		on:click={toggle}
+	>
+		<span class="block pt-40">{isPaused ? 'Start' : 'Pause'}</span>
+	</button>
 	<p class="text-8xl absolute mb-8 tracking-wider pointer-events-none">
 		{timer}
 	</p>
-	<button class="relative text-4xl tracking-[0.3em] w-full h-full rounded-full" on:click={toggle}>
-		<span class="block pt-40">{isPaused ? 'Start' : 'Pause'}</span>
-	</button>
 </div>
--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -8,6 +8,16 @@ const bgColors: BackgroundColorType = {
 	orange: 'bg-orange-400'
 };
 
+const progressColors: BackgroundColorType = {
+	pink: 'rgb(244 114 182)',
+	blue: 'rgb(56 189 248)',
+	orange: 'rgb(251 146 60)'
+};
+
 export function getBackgroundColor(color: string) {
 	return bgColors[color] ?? 'bg-pink-400';
+}
+
+export function getProgressColor(color: string) {
+	return progressColors[color] ?? 'rgb(244 114 182)';
 }
